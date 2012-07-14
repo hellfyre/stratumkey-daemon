51b453e8aaaeabfc33b5a805dee670ac35df9722
Matthias Uschok <dev@uschok.de>
2012-07-14 03:00:53 +0200

    Changed indentation

--- stratumkeySerial.py
+++ stratumkeySerial.py
@@ -5,43 +5,43 @@ import time
 import hashlib
 
 class Serial:
-  def __init__(self, port):
-    self.ser = serial.Serial(port, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, None, 0, 0, None)
-    self.ser.open()
-    self.ser.flushInput()
-    self.cipher = hashlib.sha256()
+    def __init__(self, port):
+        self.ser = serial.Serial(port, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, None, 0, 0, None)
+        self.ser.open()
+        self.ser.flushInput()
+        self.cipher = hashlib.sha256()
 
-  def __del__(self):
-    self.ser.close()
+    def __del__(self):
+        self.ser.close()
 
-  def readBytes(self, count):
-    arr = bytearray()
-    for i in range(0,count):
-      arr.append(struct.unpack('B', self.ser.read(1))[0])
-    return arr
+    def readBytes(self, count):
+        arr = bytearray()
+        for i in range(0,count):
+            arr.append(struct.unpack('B', self.ser.read(1))[0])
+        return arr
 
-  def writeBytes(self, data):
-    return self.ser.write(buffer(data))
+    def writeBytes(self, data):
+        return self.ser.write(buffer(data))
 
-  def readCommand(self):
-    cmd = struct.unpack('B', self.ser.read(1))[0]
-    return cmd
+    def readCommand(self):
+        cmd = struct.unpack('B', self.ser.read(1))[0]
+        return cmd
 
-  def readID(self):
-    d1=struct.unpack('B', self.ser.read(1))[0]
-    d2=struct.unpack('B', self.ser.read(1))[0]
-    return (d1<<8) + d2
+    def readID(self):
+        d1=struct.unpack('B', self.ser.read(1))[0]
+        d2=struct.unpack('B', self.ser.read(1))[0]
+        return (d1<<8) + d2
 
-  def flushInput(self):
-    self.ser.flushInput()
+    def flushInput(self):
+        self.ser.flushInput()
 
-  def flushOutput(self):
-    self.ser.flushOutput()
+    def flushOutput(self):
+        self.ser.flushOutput()
 
-  def openDoor(self, outputfile):
-    #self.ser.writeBytes('\x10')
-    outputfile.write('success')
-    outputfile.flush()
+    def openDoor(self, outputfile):
+        #self.ser.writeBytes('\x10')
+        outputfile.write('success')
+        outputfile.flush()
 
 #  def get_key(key_id):
 #    #TODO get the key form some database  
--- stratumkeyd.py
+++ stratumkeyd.py
@@ -7,6 +7,7 @@ import lockfile
 import hashlib
 import struct
 import threading
+import argparse
 
 import stratumkeyKeydb
 import stratumkeySerial
@@ -18,49 +19,50 @@ outputfile = None
 
 class SerialThread (threading.Thread):
 
-  def __init__(self, port, dbfile):
-    # Set up serial connection
-    # try
-    self.ser = stratumkeySerial.Serial(port)
+    def __init__(self, port, dbfile):
+        # Set up serial connection
+        # try
+        self.ser = stratumkeySerial.Serial(port)
 
-    # Set up database
-    # try
+        # Set up database
+        # try
+        self.dbfile = None
 
-    threading.Thread.__init__(self)
+        threading.Thread.__init__(self)
 
-  def run(self):
-    self.db = stratumkeyKeydb.KeyDB(dbfile)
+    def run(self):
+        self.db = stratumkeyKeydb.KeyDB(self.dbfile)
 
-    while(True):
-      command = self.ser.readCommand()
-      if (command == 0x01): # Key auth
-        cipher = hashlib.sha256()
-        id = self.ser.readID()
+        while(True):
+            command = self.ser.readCommand()
+            if (command == 0x01): # Key auth
+                cipher = hashlib.sha256()
+                id = self.ser.readID()
 
-        challenge = random.read(32)
-        self.ser.writeBytes(challenge)
-        response = self.ser.readBytes(32)
+                challenge = random.read(32)
+                self.ser.writeBytes(challenge)
+                response = self.ser.readBytes(32)
 
-        key = self.db.getKey(id)
+                key = self.db.getKey(id)
 
-        if (key != None):
-          key_and_challenge = bytearray()
-          for i in range(0,32):
-            a = struct.unpack('B', key[i])[0]
-            b = struct.unpack('B', challenge[i])[0]
-            key_and_challenge.append( struct.pack('B', (a & b)) )
+                if (key != None):
+                    key_and_challenge = bytearray()
+                    for i in range(0,32):
+                        a = struct.unpack('B', key[i])[0]
+                        b = struct.unpack('B', challenge[i])[0]
+                        key_and_challenge.append( struct.pack('B', (a & b)) )
 
-          cipher.update(key_and_challenge)
-          key_hash = cipher.digest()
+                    cipher.update(key_and_challenge)
+                    key_hash = cipher.digest()
 
-          if (response == key_hash):
-            self.ser.openDoor(outputfile)
-            self.ser.flushInput()
+                    if (response == key_hash):
+                        self.ser.openDoor(outputfile)
+                        self.ser.flushInput()
 
-      elif (command == 0x02): # Door bell
-        relayDoorBell()
+            elif (command == 0x02): # Door bell
+                relayDoorBell()
 
-      cipher = None
+            cipher = None
 
 """
 class ControlThread (threading.Thread):
@@ -70,42 +72,42 @@ class ControlThread (threading.Thread):
 
 def init():
 
-  global random
-  if os.path.exists('/dev/hwrng'):
-    random = open('/dev/hwrng', 'rb')
-  else:
-    random = open('/dev/random', 'rb')
+    global random
+    if os.path.exists('/dev/hwrng'):
+        random = open('/dev/hwrng', 'rb')
+    else:
+        random = open('/dev/random', 'rb')
 
-  global outputfile
-  outputfile = open("/var/lib/stratumkey/foobar", 'w')
-  outputfile.write("One\n")
+    global outputfile
+    outputfile = open("/var/lib/stratumkey/foobar", 'w')
+    outputfile.write("One\n")
 
 def main_loop():
-  SerialThread(args.port, args.db_file).start()
+    SerialThread(args.port, args.db_file).start()
 
 def main():
-  optparser = argparse.ArgumentParser(description="StratumKey daemon is responsible for auth'ing keys used to open the Space Gate")
-  optparser.add_argument('--no-daemon', '-n', action='store_const', const='1', help="Don't go into daemon mode")
-  optparser.add_argument('--db-file', '-d', help="The file containing the key database. Format is sqlite3", default='keydb')
-  optparser.add_argument('--port', '-p', help="Serial interface to the StratumKey master", default='/dev/ttyUSB0')
-  optparser.add_argument('--socket', '-s', help="The socket for stratumkey_ctl", default='/var/lib/stratumkey/sock_ctl')
+    optparser = argparse.ArgumentParser(description="StratumKey daemon is responsible for auth'ing keys used to open the Space Gate")
+    optparser.add_argument('--no-daemon', '-n', action='store_const', const='1', help="Don't go into daemon mode")
+    optparser.add_argument('--db-file', '-d', help="The file containing the key database. Format is sqlite3", default='keydb')
+    optparser.add_argument('--port', '-p', help="Serial interface to the StratumKey master", default='/dev/ttyUSB0')
+    optparser.add_argument('--socket', '-s', help="The socket for stratumkey_ctl", default='/var/lib/stratumkey/sock_ctl')
 
-  global args
-  args = optparser.parse_args()
+    global args
+    args = optparser.parse_args()
 
-  if not args.no_daemon:
-    d = daemon.DaemonContext()
-    d.working_directory='/var/lib/stratumkey'
-    d.pidfile=lockfile.FileLock('/var/run/stratumkey.pid')
+    if not args.no_daemon:
+        d = daemon.DaemonContext()
+        d.working_directory='/var/lib/stratumkey'
+        d.pidfile=lockfile.FileLock('/var/run/stratumkey.pid')
 
-    d.files_preserve=[outputfile]
+        d.files_preserve=[outputfile]
 
-    with d:
-      init()
-      main_loop()
-  else:
-    init()
-    main_loop()
+        with d:
+            init()
+            main_loop()
+    else:
+        init()
+        main_loop()
 
 if __name__ == "__main__":
-  main()
+    main()
