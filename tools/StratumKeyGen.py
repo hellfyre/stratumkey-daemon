5120fda4ccc8fa0d5ee70f01467c75d26d232f39
Matthias Uschok <dev@uschok.de>
2013-09-10 17:15:25 +0200

    First new version of new StratumKeyGen

--- tools/StratumKeyGen.py
+++ tools/StratumKeyGen.py
@@ -2,22 +2,56 @@
 
 import sys
 import os
+import argparse
+import uuid
+from Crypto.Cipher import AES
 
-print 'Generating random key'
+def main():
+    optparser = argparse.ArgumentParser(description="StratumKeyGen")
+    optparser.add_argument('--outputdir', '-o', help="Output directory")
 
-if os.path.exists('dev/hwrng'):
-    randomsocket = open('dev/hwrng', 'rb')
-else
-    randomsocket = open('/dev/random', 'rb')
+    args = optparser.parse_args()
 
-outputfile = open(sys.argv[1], 'w')
+    outdir = os.path.abspath(os.path.curdir)
+    if args.outputdir:
+        outdir = args.outputdir
+        if not os.path.isdir(outdir):
+            print "\"" + outdir + "\" either doesn't exist or is not a directory."
+            sys.exit(1)
+    else:
+        outdir = os.path.join(outdir, 'StratumKeyGen')
+        try:
+            os.mkdir(outdir)
+        except OSError as error:
+            print "Couldn't create \"" + outdir + "\": " + error.strerror
+            sys.exit(1)
 
-for x in range(0,32):
-    outputfile.write(randomsocket.read(1))
-    sys.stdout.write('.')
+    uuidFile = open(os.path.join(outdir, 'uuid'), 'w')
+    uuidFile.write(str(uuid.uuid4()))
+    uuidFile.close()
 
-sys.stdout.write('\n')
-randomsocket.close()
-outputfile.close()
+    randomsocket = open('/dev/urandom', 'rb')
 
-# vim: set expandtab shiftwidth=4 tabstop=4:
+    key1file = open(os.path.join(outdir, 'key1.bare'), 'w')
+    key1 = randomsocket.read(32)
+    key1file.write(key1)
+    key1file.close()
+
+    key2file = open(os.path.join(outdir, 'key2'), 'w')
+    key2 = randomsocket.read(32)
+    key2file.write(key2)
+    key2file.close()
+
+    ivfile = open(os.path.join(outdir, 'iv'), 'w')
+    iv = randomsocket.read(16)
+    ivfile.write(iv)
+    ivfile.close()
+
+    cipher = AES.new(key2, AES.MODE_CFB, iv)
+    key1cryptfile = open(os.path.join(outdir, 'key1.aes'), 'w')
+    key1crypt = cipher.encrypt(key1)
+    key1cryptfile.write(key1crypt)
+    key1cryptfile.close()
+
+if __name__ == '__main__':
+    main()
